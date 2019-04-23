ab705a7094c722ea48ecc187e1c81c7939f67179
Ryan Fickenscher <rsficken@gmail.com>
2019-04-23 16:30:47 -0700

    we are reworking everything, yay...

--- src/components/ui/action/ActionButton.js
+++ src/components/ui/action/ActionButton.js
@@ -1,26 +1,26 @@
-import React from 'react';
+// import React from 'react';
 
-import PillButton from '../pill/PillButton';
+// import PillButton from '../pill/PillButton';
 
-import actionButtonCSS from './ActionButton.module.css';
+// import actionButtonCSS from './ActionButton.module.css';
 
-const actionButton = (props) => {
-    let value = props.values[0];
-    if (props.state) {
-        value = props.values[1];
-    }
+// const actionButton = (props) => {
+//     let value = props.values[0];
+//     if (props.state) {
+//         value = props.values[1];
+//     }
 
-    const handle_onClick = () => {
-        props.onClick();
-    }
+//     const handle_onClick = () => {
+//         props.onClick();
+//     }
 
-    return (
-        <PillButton
-            className={actionButtonCSS.Action_Button}
-            onClick={handle_onClick}>
-            {value}
-        </PillButton>
-    );
-}
+//     return (
+//         <PillButton
+//             className={actionButtonCSS.Action_Button}
+//             onClick={handle_onClick}>
+//             {value}
+//         </PillButton>
+//     );
+// }
 
-export default actionButton;
+// export default actionButton;
--- src/containers/Inspector/Collection/CollectionInspector.js
+++ src/containers/Inspector/Collection/CollectionInspector.js
@@ -1,4 +1,13 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+import * as actions from '../../../store/actions/index';
+
+import Aux from '../../../hoc/aux/Aux';
+import Header from '../../../components/ui/Header/Header';
+import Throbber from '../../../components/ui/throbber/Throbber';
+import List from '../../../components/ui/list/List/List';
+import ActionButton from '../../../components/ui/action/ActionButton';
 
 class CollectionInspector extends Component {
     render () {
