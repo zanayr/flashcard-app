4ee9e9814660183240991b9e55743feb783e075c
Ryan Fickenscher <rsficken@gmail.com>
2019-06-04 13:54:18 -0700

    alt

--- src/containers/Alt/Alt.js
+++ src/containers/Alt/Alt.js
@@ -2,16 +2,18 @@ import React from 'react';
 
 import {Link} from 'react-router-dom';
 
+import styles from '../Container.module.css';
+
 
 const alt = (props) => {
     return (
-        <main>
+        <main className={styles.Alt}>
             <div>
                 <section>
                     <div>
                         <h1>There was an issue loggin you in!</h1>
-                        <p>{props.location.state.message} Sorry for the inconvenience.</p>
-                        <p>Click here to return to the <Link to='/out'>sign in</Link> page.</p>
+                        <p>{props.location.state.message} We appologize for the inconvenience.</p>
+                        <p>Return to the <Link to='/out'>sign in</Link> page.</p>
                     </div>
                 </section>
             </div>
