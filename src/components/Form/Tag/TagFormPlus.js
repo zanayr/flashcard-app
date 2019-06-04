caf85bff7c74f68ceb5b06acd93db4f60cdcb631
Ryan Fickenscher <rsficken@gmail.com>
2019-06-04 12:11:54 -0700

    minor

--- src/components/Form/Tag/TagFormPlus.js
+++ src/components/Form/Tag/TagFormPlus.js
@@ -94,6 +94,7 @@ class TagFormPlus extends Component {
                             maxLength: 24,
                             minLength: 3,
                             name: 'tag',
+                            pattern: '[a-zA-z0-9 -.]+',
                             tabIndex: this.props.tabIndex
                         }}
                         value={this.state.value}
