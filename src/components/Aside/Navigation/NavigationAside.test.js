fce1f9ee1d9c6d03b36f6ee579c5af39124504b1
Ryan Fickenscher <rsficken@gmail.com>
2019-06-05 12:36:33 -0700

    added 2 nav tests

--- src/components/Aside/Navigation/NavigationAside.test.js
+++ src/components/Aside/Navigation/NavigationAside.test.js
@@ -9,8 +9,12 @@ import BarButton from '../../ui/button/Bar/BarButton';
 configure({adapter: new Adapter()});
 
 describe ('<NavigationAside/>', () => {
-    it('should render the users <BarButton/> if the user has the privilage', () => {
+    it('should render five <BarButton/> components if the user is an admin.', () => {
         const wrapper = shallow(<NavigationAside select_user={{privilage: 1}}/>);
         expect(wrapper.find(BarButton)).toHaveLength(5);
+    });
+    it('should render four <BarButton/> components if the user\'s prvileges is only 0', () => {
+        const wrapper = shallow(<NavigationAside select_user={{privilage: 0}}/>);
+        expect(wrapper.find(BarButton)).toHaveLength(4);
     });
 });
